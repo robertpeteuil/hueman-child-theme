55e6d0753db97fe886edbb30bee3605520ad8418
Robert Peteuil <robert@peteuil.com>
2017-03-04 16:58:56 -0700

    Added functions to add a LinkedIn profile meta to each user Reorganizer order, end-user related functions on top, admin related on bottom

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-/* enque parent stylesheet into shild-theme */
+/* enque parent stylesheet into child-theme */
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
@@ -12,17 +12,8 @@ function my_theme_enqueue_styles() {
     );
 }
 
-/* WHide Dashboard View from all users by default */
-add_action("user_register", "set_user_admin_bar_false_by_default", 10, 1);
-function set_user_admin_bar_false_by_default($user_id) {
-    update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
-    update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
-}
-
-// Stop jetpack from placing social sharing butttons inside content
-//    It does this by default by adding these from inside the_content() function
-// This function stops it from automatically displaying them
-//    Then you need to put function on page where you want them displayed.
+// Stop jetpack from placing social sharing butttons inside he_content() function
+//    You need to add another php function on page to display them
 //    described here:  https://jetpack.com/2013/06/10/moving-sharing-icons/
 function jptweak_remove_share() {
     remove_filter( 'the_content', 'sharing_display',19 );
@@ -32,6 +23,22 @@ function jptweak_remove_share() {
     }
 }
 add_action( 'loop_start', 'jptweak_remove_share' );
+
+/* Hide Dashboard View from all users by default */
+add_action("user_register", "set_user_admin_bar_false_by_default", 10, 1);
+function set_user_admin_bar_false_by_default($user_id) {
+    update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
+    update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
+}
+
+/* Add linkedin field to user metadata */
+function add_to_author_profile( $contactmethods ) {
+  $contactmethods['linkedin'] = 'LinkedIn profile URL';
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'add_to_author_profile', 10, 1);
+
+// ADMIN RELATED CHANGES BELOW THIS LINE
 
 // remove admin toolbar items
 // https://digwp.com/2016/06/remove-toolbar-items/
@@ -49,14 +56,3 @@ function shapeSpace_remove_toolbar_menu() {
 	$wp_admin_bar->remove_menu('wpaas');                // GoDaddy Branding Logo
 }
 add_action('wp_before_admin_bar_render', 'shapeSpace_remove_toolbar_menu', 999);
-
-// Allow HTML in user bios is documented here
-// TURNED OFF - security risk unless users are locked out of editing bios
-//    because with HTML turned on they could do some bad things
-// http://wordpress.stackexchange.com/questions/21326/how-can-html-be-allowed-in-author-bio
-// https://premium.wpmudev.org/blog/enable-or-disable-all-html-tags-in-wordpress-author-biography-profiles/
-//
-// remove the standard filter which strips most HTML from bios
-// remove_filter('pre_user_description', 'wp_filter_kses');
-//add sanitization for WordPress posts
-// add_filter( 'pre_user_description', 'wp_filter_post_kses');
