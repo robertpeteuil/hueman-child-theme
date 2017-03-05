5b87756ea8d264e2ea5da824fe80e9198c30ac35
Robert Peteuil <robert@peteuil.com>
2017-03-04 19:57:20 -0700

    functions - new method of calling CSS

--- functions.php
+++ functions.php
@@ -1,19 +1,31 @@
 <?php
 
-/* enque parent stylesheet into child-theme */
+/* enque parent stylesheet into shild-theme */
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/assets/front/css/main.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
+        // array( $parent_style ),
+        array(),
+        // wp_get_theme()->get('Version')
+        rand(111,9999), 'all'
     );
 }
+// wp_register_style( 'custom-style', get_template_directory_uri() . '/css/custom-style.css', array(), rand(111,9999), 'all' );
 
-// Stop jetpack from placing social sharing butttons inside he_content() function
-//    You need to add another php function on page to display them
+/* WHide Dashboard View from all users by default */
+add_action("user_register", "set_user_admin_bar_false_by_default", 10, 1);
+function set_user_admin_bar_false_by_default($user_id) {
+    update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
+    update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
+}
+
+// Stop jetpack from placing social sharing butttons inside content
+//    It does this by default by adding these from inside the_content() function
+// This function stops it from automatically displaying them
+//    Then you need to put function on page where you want them displayed.
 //    described here:  https://jetpack.com/2013/06/10/moving-sharing-icons/
 function jptweak_remove_share() {
     remove_filter( 'the_content', 'sharing_display',19 );
@@ -23,22 +35,6 @@ function jptweak_remove_share() {
     }
 }
 add_action( 'loop_start', 'jptweak_remove_share' );
-
-/* Hide Dashboard View from all users by default */
-add_action("user_register", "set_user_admin_bar_false_by_default", 10, 1);
-function set_user_admin_bar_false_by_default($user_id) {
-    update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
-    update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
-}
-
-/* Add linkedin field to user metadata */
-function add_to_author_profile( $contactmethods ) {
-  $contactmethods['linkedin'] = 'LinkedIn profile URL';
-  return $contactmethods;
-}
-add_filter( 'user_contactmethods', 'add_to_author_profile', 10, 1);
-
-// ADMIN RELATED CHANGES BELOW THIS LINE
 
 // remove admin toolbar items
 // https://digwp.com/2016/06/remove-toolbar-items/
@@ -56,3 +52,10 @@ function shapeSpace_remove_toolbar_menu() {
 	$wp_admin_bar->remove_menu('wpaas');                // GoDaddy Branding Logo
 }
 add_action('wp_before_admin_bar_render', 'shapeSpace_remove_toolbar_menu', 999);
+
+/* Add linkedin field to user metadata */
+function add_to_author_profile( $contactmethods ) {
+  $contactmethods['linkedin'] = 'LinkedIn profile URL';
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'add_to_author_profile', 10, 1);
