94de738c5239c2ae923409c7ecfaadd9a141b8b5
Robert Peteuil <robert@peteuil.com>
2017-03-12 10:12:51 -0700

    parts/page-title - use of Yoast breadcrumb in title is now conditional.  it now executes normal theme code if Yoast breadcrumbs are not installed/enabled.

--- parts/page-title.php
+++ parts/page-title.php
@@ -3,19 +3,25 @@
 	<?php if ( is_home() && hu_is_checked('blog-heading-enabled') ) : ?>
 		<h2><?php echo hu_blog_title(); ?></h2>
 	<?php elseif ( is_single() ): ?>
-    <?php if ( function_exists('yoast_breadcrumb') ) {
-      yoast_breadcrumb(' <p id="breadcrumbs">','</p> '); } ?>
-		<!-- <ul class="meta-single group">
-			<li class="category"><?php the_category(' <span>/</span> '); ?></li>
-			<?php if ( comments_open() && ( hu_is_checked( 'comment-count' ) ) ): ?>
-			<li class="comments"><a href="<?php comments_link(); ?>"><i class="fa fa-comments-o"></i><?php comments_number( '0', '1', '%' ); ?></a></li>
-			<?php endif; ?> -->
-		</ul>
+    <?php if ( function_exists('yoast_breadcrumb') ): ?>
+      <?php yoast_breadcrumb(' <p id="breadcrumbs">','</p> '); ?>
+    <?php else: ?>
+  		<ul class="meta-single group">
+  			<li class="category"><?php the_category(' <span>/</span> '); ?></li>
+  			<?php if ( comments_open() && ( hu_is_checked( 'comment-count' ) ) ): ?>
+  			<li class="comments"><a href="<?php comments_link(); ?>"><i class="fa fa-comments-o"></i><?php comments_number( '0', '1', '%' ); ?></a></li>
+  			<?php endif; ?>
+		  </ul>
+    <?php endif; ?>
 
 	<?php elseif ( is_page() ): ?>
-    <?php if ( function_exists('yoast_breadcrumb') ) {
-      yoast_breadcrumb(' <p id="breadcrumbs">','</p> '); } ?>
-		<!-- <h2><?php echo hu_get_page_title(); ?></h2> -->
+    <!-- <?php if ( function_exists('yoast_breadcrumb') ) {
+      yoast_breadcrumb(' <p id="breadcrumbs">','</p> '); } ?> -->
+    <?php if ( function_exists('yoast_breadcrumb') ): ?>
+      <?php yoast_breadcrumb(' <p id="breadcrumbs">','</p> '); ?>
+    <?php else: ?>
+		    <h2><?php echo hu_get_page_title(); ?></h2>
+    <?php endif; ?>
 	<?php elseif ( is_search() ): ?>
 		<h1><?php echo hu_get_search_title(); ?></h1>
 	<?php elseif ( is_404() ): ?>
