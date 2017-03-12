fcbb7432025d4804842abee8484530524886ae5b
Robert Peteuil <robert@peteuil.com>
2017-03-12 10:55:34 -0700

    functions.php  - clean up comments

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-/* enque my stylesheet and override parent theme's enque of it */
+// enque my stylesheet and override parent theme's enqueue of it
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css', array( 'hueman-main-style' ),
@@ -10,14 +10,14 @@ function my_theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 999 );
 
-/* Whide Dashboard View from all users by default */
+// hide dashboard view from users by default
 add_action("user_register", "set_user_admin_bar_false_by_default", 10, 1);
 function set_user_admin_bar_false_by_default($user_id) {
     update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
     update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
 }
 
-// Stop jetpack from placing social sharing butttons inside content
+// stop jetpack social sharing butttons from displaying inside the_content
 function jptweak_remove_share() {
     remove_filter( 'the_content', 'sharing_display',19 );
     remove_filter( 'the_excerpt', 'sharing_display',19 );
@@ -27,24 +27,24 @@ function jptweak_remove_share() {
 }
 add_action( 'loop_start', 'jptweak_remove_share' );
 
-// remove admin toolbar items
+// remove admin toolbar items via remove_node
 function RP_remove_toolbar_node($wp_admin_bar) {
-  $wp_admin_bar->remove_node('wp-logo');              // WordPress Logo
-  $wp_admin_bar->remove_node('new-content');          // Add new content
-  $wp_admin_bar->remove_node('stats');                // Svr Load Bargraph
+  $wp_admin_bar->remove_node('wp-logo');
+  $wp_admin_bar->remove_node('new-content');
+  $wp_admin_bar->remove_node('stats');
 }
 add_action('admin_bar_menu', 'RP_remove_toolbar_node', 999);
 
-// remove admin toolbar items - part 2 - alt technique
+// remove admin toolbar items via remove_menu
 function RP_remove_toolbar_menu() {
 	global $wp_admin_bar;
-  $wp_admin_bar->remove_menu('wpseo-menu');           // Yoast SEO
-  $wp_admin_bar->remove_menu('ybi_plugin_shortcut');  // Upload Plugin
-	$wp_admin_bar->remove_menu('wpaas');                // GoDaddy Logo
+  $wp_admin_bar->remove_menu('wpseo-menu');
+  $wp_admin_bar->remove_menu('ybi_plugin_shortcut');
+	$wp_admin_bar->remove_menu('wpaas');
 }
 add_action('wp_before_admin_bar_render', 'RP_remove_toolbar_menu', 999);
 
-/* Add linkedin field to user metadata */
+// add LinkedIn field to user metadata 
 function add_to_author_profile( $contactmethods ) {
   $contactmethods['linkedin'] = 'LinkedIn profile URL';
   return $contactmethods;
